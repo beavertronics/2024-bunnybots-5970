a9ca2e2fa5e71644aeffd740236e98c99f8d83e1
Will Kam <william@kamelot.me>
2024-12-13 20:49:06 -0800

    Fix runIntake bug

--- src/main/kotlin/frc/robot/subsystems/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/Intake.kt
@@ -24,11 +24,11 @@ object Intake : SubsystemBase() {
     }
 
     fun runIntake(speed: VoltageUnit) {
-        intakeMotor.setVoltage(speed)
+        intakeMotor.setVoltage(speed.asVolts)
     }
 
     fun runConveyor(speed: VoltageUnit) {
-        conveyorMotor.setVoltage(speed)
+        conveyorMotor.setVoltage(speed.asVolts)
     }
 
     fun raiseIntake() {
